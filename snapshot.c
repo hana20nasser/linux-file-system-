6304ba5e5b81da4f96fee43df6e5837c007e2086
Bola Ghaly <bolagerges221@gmail.com>
2026-05-17 23:25:38 +0300

    snapshot.c edited to match info and delete

--- snapshot.c
+++ snapshot.c
@@ -1,60 +1,73 @@
-#include "../project/utils.c"
-#include "../project/scanner.c"
-#include "../project/copy.c"
-#include "../project/storage.c"
-#include "../project/compare.c"
-#include "../project/restore.c"
+#include "kernel/types.h"
+#include "user/user.h"
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        printf("Usage: snapshot [command] [args]\n");
-        printf("Commands:\n  scan [path]\n  create [snapshot_name] [path]\n  compare [snapshot_name]\n  restore [snapshot_name]\n");
-        exit(0);
-    }
+// Include all modular component interfaces
+#include "scanner.h"
+#include "copy.h"
+#include "storage.h"
+#include "compare.h"
+#include "restore.h"
+#include "info.h"
+#include "delete.h"
 
-    char *command = argv[1];
-
-    if (strcmp(command, "scan") == 0)
-    {
-        char *path = (argc > 2) ? argv[2] : ".";
-        run_scan(path);
-    }
-    else if (strcmp(command, "create") == 0)
-    {
-        if (argc < 3)
-        {
-            printf("Error: Provide snapshot name. (e.g., snapshot create v1 demo)\n");
-            exit(0);
-        }
-
-        char *target = (argc > 3) ? argv[3] : ".";
-
-        run_create(argv[2], target);
-    }
-    else if (strcmp(command, "compare") == 0)
-    {
-        if (argc < 3)
-        {
-            printf("Error: Provide snapshot to compare. (e.g., snapshot compare v1)\n");
-            exit(0);
-        }
-        run_compare(argv[2]);
-    }
-    else if (strcmp(command, "restore") == 0)
-    {
-        if (argc < 3)
-        {
-            printf("Error: Provide snapshot to restore. (e.g., snapshot restore v1)\n");
-            exit(0);
-        }
-        run_restore(argv[2]);
-    }
-    else
-    {
-        printf("Unknown command: %s\n", command);
-    }
-
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    printf("Usage: snapshot [command] [args]\n");
+    printf("Commands:\n");
+    printf("  scan [path]\n");
+    printf("  create [name] [target_dir]\n");
+    printf("  compare [name]\n");
+    printf("  restore [name]\n");
+    printf("  info [name]\n");
+    printf("  delete [name]\n");
     exit(0);
+  }
+
+  char *command = argv[1];
+
+  if(strcmp(command, "scan") == 0) {
+    char *path = (argc > 2) ? argv[2] : ".";
+    run_scan(path);
+  } 
+  else if(strcmp(command, "create") == 0) {
+    if(argc < 3) {
+      printf("Error: Provide snapshot name. (e.g., snapshot create v1 .)\n");
+      exit(0);
+    }
+    char *target = (argc > 3) ? argv[3] : ".";
+    run_create(argv[2], target);
+  } 
+  else if(strcmp(command, "compare") == 0) {
+    if(argc < 3) {
+      printf("Error: Provide snapshot name to compare.\n");
+      exit(0);
+    }
+    run_compare(argv[2]);
+  } 
+  else if(strcmp(command, "restore") == 0) {
+    if(argc < 3) {
+      printf("Error: Provide snapshot name to restore.\n");
+      exit(0);
+    }
+    run_restore(argv[2]);
+  } 
+  else if(strcmp(command, "info") == 0) {
+    if(argc < 3) {
+      printf("Error: Provide snapshot name to view.\n");
+      exit(0);
+    }
+    run_info(argv[2]);
+  } 
+  else if(strcmp(command, "delete") == 0) {
+    if(argc < 3) {
+      printf("Error: Provide snapshot name to purge.\n");
+      exit(0);
+    }
+    run_delete(argv[2]);
+  } 
+  else {
+    printf("Unknown command: %s\n", command);
+  }
+
+  exit(0);
 }
